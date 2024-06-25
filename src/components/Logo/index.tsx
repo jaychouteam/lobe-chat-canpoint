f3cc3f4e48d54da258afa39876598e34149965b9
wihimi <18807242213@163.com>
2024-06-25 08:34:33 +0800

    1

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
-import { Image } from '@lobehub/ui';
+// import { Image } from '@lobehub/ui';
 
-import { imageUrl } from '@/const/url';
+// import { imageUrl } from '@/const/url';
 // import { number } from 'zod';
 interface ModelTagProps {
   // model: string;
@@ -9,10 +9,12 @@ interface ModelTagProps {
 }
 const Logo = memo<ModelTagProps>(({ width = 150 }) => {
   return (
-    <Image
-      style={{ width: width }}
-      src={imageUrl('canpoint.svg')}
-    />
+    <div style={{ fontSize: '18px' }} >CANPOINT</div>
+    // <Image
+    //   alwaysShowActions={false}
+    //   style={{ width: width }}
+    //   src={imageUrl('canpoint.svg')}
+    // />
     // <img src={imageUrl('canpoint.svg')} width={width} alt="" />
   );
 });
