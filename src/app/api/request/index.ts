46e7ae97f280449e6cd178b5d6ae70f23806ff28
wihimi <18807242213@163.com>
2024-06-26 10:04:38 +0800

    测试ios 登录跳转

--- src/app/api/request/index.ts
+++ src/app/api/request/index.ts
@@ -6,31 +6,40 @@ getAuth().then(res=>{
 })
  * 
  * 
- *  */ 
+ *  */
 // import {getToken} from '@/utils/cookie'
 import cookie from 'react-cookies'
 
+export const jumpLogin =()=> {
+    let url = encodeURIComponent(window.location.href) //decodeURIComponent(window.location.href)
+    console.log(window.location.href, url);
+    cookie.save('URLTOKEN', url, { domain: '.canpoint.cn', path: "/" })
+    // window.location.href = 'http://account.canpoint.cn/#/login?isIndex=account';
+    setTimeout(() => {
+        window.open('//account.canpoint.cn/#/login?isIndex=account', '_blank')
+    }, 0)
+}
+
 export const getAuth = () => {
-    let token=  cookie.load('CANPOINTTOKEN')
-    //??'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyR3VpZCI6IlQyTXpiak5uTWpBMWJqUm5aMEZ1UlRkUGJsRTVVVDA5IiwiZXhwIjoxNzE4NjAxMjk4fQ.e2ZVENqgo6ueqieMCp1hMt6_01l5yC9RRhjtOMLvshw'
+    let token = cookie.load('CANPOINTTOKEN')
     return new Promise<boolean>((resolve) => {
-        if(!token) resolve(false)
+        if (!token) {
+            jumpLogin()
+            resolve(false)
+            return
+        }
         fetch('http://canpoint-cloud-api.canpoint.cn/auth/checkCompanyEmployees', {
             headers: {
-                Canpointtoken:token//getToken//'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyR3VpZCI6IlQyTXpiak5uTWpBMWJqUm5aMEZ1UlRkUGJsRTVVVDA5IiwiZXhwIjoxNzE4NjAxMjk4fQ.e2ZVENqgo6ueqieMCp1hMt6_01l5yC9RRhjtOMLvshw'
+                Canpointtoken: token//getToken//'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyR3VpZCI6IlQyTXpiak5uTWpBMWJqUm5aMEZ1UlRkUGJsRTVVVDA5IiwiZXhwIjoxNzE4NjAxMjk4fQ.e2ZVENqgo6ueqieMCp1hMt6_01l5yC9RRhjtOMLvshw'
             }
         })
             .then(response => response.json())
             .then(data => {
-                console.log(2,data);
-                if(data.code===200){
+                console.log(2, data);
+                if (data.code === 200) {
                     resolve(true)
-                }else{
-                    let url= encodeURIComponent(window.location.href) //decodeURIComponent(window.location.href)
-                    console.log(window.location.href,url);
-                    cookie.save('URLTOKEN',url ,{domain:'.canpoint.cn',path:"/"})
-                    // window.location.href = 'http://account.canpoint.cn/#/login?isIndex=account';
-                    window.open ( '//account.canpoint.cn/#/login?isIndex=account' , '_blank' ) 
+                } else {
+                    jumpLogin()
                     resolve(false)
                 }
                 // 处理获取到的数据
@@ -41,8 +50,8 @@ export const getAuth = () => {
     })
 
 }
- 
- 
+
+
 
 
 
